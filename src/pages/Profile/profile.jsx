a95091817c9eb9598f6dfdb6fbe851e3c836c052
mabelam <mabellam8828@gmail.com>
2023-08-29 02:40:50 -0700

    userprofile css

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Button, TextField, Typography } from '@mui/material';
-import { updatePassword } from '../../utilities/users-service';
-
+import { updatePassword, getUser } from '../../utilities/users-service';
+import './profile.css';
 function UserProfile() {
   const [showChangePasswordForm, setShowChangePasswordForm] = useState(false);
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [user, setUser] = useState(getUser());
 
   const handleSubmitChangePassword = async (event) => {
     event.preventDefault();
@@ -37,10 +38,32 @@ function UserProfile() {
 
   return (
     <div className="user-profile">
+      <img src="https://lh3.googleusercontent.com/pw/AIL4fc_jjScx0Re4L0kycicvXQvVzp0FWSloeimCAWY5H8qSk-yShPakaklgGNzvM2aVEF0FS0GJ3BFuYNwGzP60ViUOP6FuaXd021pbdoklAxMDxGv5cYY=w2400" alt="image"  />
+        <Typography variant="h4" gutterBottom>
+          Welcome, {user.name}!
+        </Typography>
+
+        <Typography variant="body2" gutterBottom>
+          Email: {user.email}
+        </Typography>
+        {!showChangePasswordForm && (
+          <Button
+            onClick={() => setShowChangePasswordForm(true)}
+            variant="contained"
+            halfWidth
+            style={{
+              backgroundColor: '#ADA9FC',
+              color: 'white',
+              marginTop: '2vmin',
+            }}
+          >
+            Change Password
+          </Button>
+        )}
       <Button
         onClick={handleGoBackHome}
         variant="contained"
-        fullWidth
+        halfWidth
         style={{
           backgroundColor: '#ADA9FC',
           color: 'white',
@@ -50,20 +73,7 @@ function UserProfile() {
         Home Page
       </Button>
 
-      {!showChangePasswordForm && (
-        <Button
-          onClick={() => setShowChangePasswordForm(true)}
-          variant="contained"
-          fullWidth
-          style={{
-            backgroundColor: '#ADA9FC',
-            color: 'white',
-            marginTop: '2vmin',
-          }}
-        >
-          Change Password
-        </Button>
-      )}
+
 
       {showChangePasswordForm && (
         <form onSubmit={handleSubmitChangePassword}>
